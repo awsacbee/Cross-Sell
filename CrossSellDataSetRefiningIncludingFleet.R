01c505414a17c0704c47729a6e98d7a73fbb05b1
Welden <awelden@sacbee.com>
2016-06-14 13:53:46 -0700

    commented out

--- CrossSellDataSetRefiningIncludingFleet.R
+++ CrossSellDataSetRefiningIncludingFleet.R
@@ -95,6 +95,6 @@ write.csv(CrossSellRefined, "C:/Users/awelden/Google Drive/MAD Science/Internal 
 
 
 ### Folsom Lake Honda only
-table(grep("Folsom Lake", CrossSellRefined$SELLER, ignore.case=T, value=T))
-head(CrossSellRefined[CrossSellRefined$SELLER=="FOLSOM LAKE HONDA",])
-write.csv(CrossSellRefined[CrossSellRefined$SELLER=="FOLSOM LAKE HONDA",], "C:/Users/awelden/Google Drive/MAD Science/Internal Tools/Tableau/Folsom Lake Honda/FLH.csv")
+#table(grep("Folsom Lake", CrossSellRefined$SELLER, ignore.case=T, value=T))
+#head(CrossSellRefined[CrossSellRefined$SELLER=="FOLSOM LAKE HONDA",])
+#write.csv(CrossSellRefined[CrossSellRefined$SELLER=="FOLSOM LAKE HONDA",], "C:/Users/awelden/Google Drive/MAD Science/Internal Tools/Tableau/Folsom Lake Honda/FLH.csv")
